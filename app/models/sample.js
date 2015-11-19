629965c7593548e734af36cc17efb53380fec7cf
William Li <wli@williamhli.com>
2015-11-18 16:10:24 -0800

    Add eslint config

--- app/models/sample.js
+++ app/models/sample.js
@@ -1,7 +1,7 @@
-import Ember from "ember";
+import Ember from 'ember';
 
 export default Ember.Object.extend({
   thisDoesThings: function() {
-      return this.get('name') + ' is pretty cool!';
+    return this.get('name') + ' is pretty cool!';
   }.property('name')
 });
